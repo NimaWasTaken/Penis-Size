5bcbe2fa251a813251b08e41416a0d811a245d58
NimaWasTaken <100118467+NimaWasTaken@users.noreply.github.com>
2022-02-21 11:11:49 +0330

    Add files via upload
    This works like the "Dank Memer" discord bot.

---
+++ Sussy Baka.py
@@ -0,0 +1,8 @@
+import random
+
+Name = input("Please enter your name: ")
+PPBody = "===================="
+Size = random.randint(1,20)
+PP = "".join(random.sample(PPBody,Size))
+print(Name+"'s penis: ")
+print("8"+PP+"D")
